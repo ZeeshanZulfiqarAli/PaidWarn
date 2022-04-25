7dc26215171dec37360837e2c8f02046903a3ef6
themanmaran <tyler.maran@gmail.com>
2022-04-25 11:17:11 -0400

    testing with regex generator

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
 # PaidWarn
+
 Google chrome extension to warn if the url is known to be behind a paywall
 
 ## Usage
+
 To use this extension, you need:
+
 1. Clone the repo
-2. Enable developers mode in extensions page 
+2. Enable developers mode in extensions page
 3. Click on Load unpacked button:
-  - Navigate to the repo
-  - Click Select button
+
+-   Navigate to the repo
+-   Click Select button
 
 Now the extension should be enabled and work on every website.
+
+Test URLs:
+
+https://www.nytimes.com/
+
+https://glassdoor.com/
 
 Note: Currently as this is under development, usage is not recommended and may hamper web experience
--- content.js
+++ content.js
@@ -1,7 +1,21 @@
-const blacklist = /google\.com|twitter\.com/;
+const blacklist = [
+	'economist.com',
+	'fortune.com',
+	'glassdoor.com',
+	'nytimes.com',
+	'washingtonpost.com',
+	'wired.com',
+];
 
-document.querySelectorAll('a').forEach(node => {
-    if (blacklist.test(node.href)) {
-        node.innerText += ' [PAID]';
-    }
+let regex = blacklist.reduce((acc, el) => {
+	return (acc += `${el.split('.')[0]}\\.${el.split('.')[1]}|`);
+}, '');
+regex = new RegExp(regex.slice(0, -1));
+
+// console.log('regex', regex);
+
+document.querySelectorAll('a').forEach((node) => {
+	if (regex.test(node.href)) {
+		node.innerText += ' [PAID]';
+	}
 });
